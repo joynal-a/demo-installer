9d937c8ee0207dc5371e9df1cf27e43f5432fdf8
Joynal Abedin <abedin.dev@gmail.com>
2025-02-03 18:37:25 +0600

    update loader image

--- resources/views/install.blade.php
+++ resources/views/install.blade.php
@@ -5,7 +5,7 @@
     <div class="col-6 col-md-4 ms-auto d-flex align-items-center me-5" style="height: 100vh;" id="mainCard">
         <div class="card w-100 pt-3" style="overflow: hidden;border-bottom: none;">
             <div class="loader" id="loader" style="display: none">
-                <img src="https://i.ibb.co.com/0zKw847/echomart-logo.png" alt="cog" border="0">
+                <img src="https://i.ibb.co.com/GQ2TkNBd/cog-loader.gif" alt="cog" border="0">
             </div>
 
             <h2 class="text-center mt-3">Ready For Installation</h2>
